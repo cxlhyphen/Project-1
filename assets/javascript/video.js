14d54f3d755effa64cd22207af6d1d904dd27c21
PhelanM <phelanmccall@yahoo.com>
2018-11-01 20:47:37 -0700

    update index with player and ids, video js makes succesful api call

--- assets/javascript/video.js
+++ assets/javascript/video.js
@@ -1,3 +1,22 @@
 $(document).ready(function(){
 
+ 
+    $("#submit").on("click", function(e){
+        e.preventDefault();
+
+        var type = $("#input").val().trim();
+        var queryURL = "https://www.googleapis.com/youtube/v3/search?part=snippet&q=" + type + "&type=video&key=AIzaSyDtZJ5dO2lCwvyPRmwTKVeWNKMQRpI31qg";
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function(response){
+             console.log(response.items[0].id.videoId);
+         });
+
+    });
+
+
+
+
 });
