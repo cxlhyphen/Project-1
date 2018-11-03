0642fdf615ac8fb0bc31f59978bf701a0cadb545
Sean <seanpitfield@gmail.com>
2018-11-03 10:20:52 -0700

    deleted index.3

--- assets/javascript/video.js
+++ assets/javascript/video.js
@@ -1,43 +1,24 @@
 $(document).ready(function(){
 
 });
- // 2. This code loads the IFrame Player API code asynchronously.
- var tag = document.createElement('script');
-        
- tag.src = "https://www.youtube.com/iframe_api";
- var firstScriptTag = document.getElementsByTagName('script')[0];
- firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+$("#submit").on("click", function(e){
+  e.preventDefault();
 
- // 3. This function creates an <iframe> (and YouTube player)
- //    after the API code downloads.
- var player;
- function onYouTubeIframeAPIReady() {
-   player = new YT.Player('player', {
-     height: '390',
-     width: '640',
-     videoId: 'M7lc1UVf-VE',
-     events: {
-       'onReady': onPlayerReady,
-       'onStateChange': onPlayerStateChange
-     }
+  var type = $("#input").val().trim();
+  var queryURL = "https://www.googleapis.com/youtube/v3/search?part=snippet&q=" + type + "&type=video&key=AIzaSyDtZJ5dO2lCwvyPRmwTKVeWNKMQRpI31qg";
+
+  $.ajax({
+      url: queryURL,
+      method: "GET"
+  }).then(function(response){
+       console.log(response.items[0].id.videoId);
    });
- }
 
- // 4. The API will call this function when the video player is ready.
- function onPlayerReady(event) {
-   event.target.playVideo();
- }
+});
 
- // 5. The API calls this function when the player's state changes.
- //    The function indicates that when playing a video (state=1),
- //    the player should play for six seconds and then stop.
- var done = false;
- function onPlayerStateChange(event) {
-   if (event.data == YT.PlayerState.PLAYING && !done) {
-     setTimeout(stopVideo, 6000);
-     done = true;
-   }
- }
- function stopVideo() {
-   player.stopVideo();
- }
+
+
+
+
+
+      
