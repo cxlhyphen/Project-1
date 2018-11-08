6b0a13966d923e41c15a6a85b5710e96f6dd3b35
PhelanM <phelanmccall@yahoo.com>
2018-11-07 21:51:13 -0800

    local storage up and running. might need a way to delete or limit the number of links

--- assets/javascript/video.js
+++ assets/javascript/video.js
@@ -1,6 +1,95 @@
+function storageAvailable(type) {
+  try {
+    var storage = window[type],
+      x = '__storage_test__';
+    storage.setItem(x, x);
+    storage.removeItem(x);
+    return true;
+  }
+  catch (e) {
+    return e instanceof DOMException && (
+      // everything except Firefox
+      e.code === 22 ||
+      // Firefox
+      e.code === 1014 ||
+      // test name field too, because code might not be present
+      // everything except Firefox
+      e.name === 'QuotaExceededError' ||
+      // Firefox
+      e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
+      // acknowledge QuotaExceededError only if there's something already stored
+      storage.length !== 0;
+  }
+}
+
+var links = [];
+
+function setLinks() {
+
+
+  var string = localStorage.getItem("videoLinks");
+
+  links = string.split(" ");
+  console.log(links);
+  displayHistory();
+}
+
+function addStorage() {
+
+  localStorage.setItem("videoLinks", links.join(" "));
+
+  setLinks();
+
+  console.log(localStorage.getItem("videoLinks"))
+}
+
+if (storageAvailable('localStorage')) {
+  console.log(!localStorage.getItem("videoLinks"));
+  // Yippee! We can use localStorage awesomeness
+  if (!localStorage.getItem("videoLinks")) {
+    addStorage();
+  } else {
+    setLinks();
+  }
+
+}
+else {
+  // Too bad, no localStorage for us
+}
+
+function displayHistory() { 
+  console.log("DISPLAYING")
+
+  $("#trackName").html("");
+  console.log(links)
+  for (let i in links) {
+
+    if (links[i].length > 5 && links[i].length < 20) {
+
+      var data = $("<td>")
+      var link = $("<a>");
+      link.attr("href", "https://www.youtube.com/watch?v=" + links[i]);
+      link.attr("target", "_blank")
+      link.addClass("col-4");
+
+      //  var title = $("<p>");
+      //title.addClass("col-2")
+      // link.text(song.snippet.title);
+      var img = $("<img>");
+      img.addClass("img-responsive");
+      img.attr("src", "https://i.ytimg.com/vi/" + links[i] + "/default.jpg");
+      //link.append(title);
+
+      link.append(img);
+      data.append(link);
+      $("#trackName").prepend(data);
+
+    }
+  }
+}
 
 function callYoutubeSearchApi(artist, track) {
-  var type = '"' + artist + '" ' + "+" + '"' + track + '"' ;
+  var type = '"' + artist + '" ' + "+" + '"' + track + '"';
   var queryURL = "https://www.googleapis.com/youtube/v3/search?part=snippet&q=" + type + "&type=video&key=AIzaSyDtZJ5dO2lCwvyPRmwTKVeWNKMQRpI31qg";
   console.log("CALLLING")
   $.ajax({
@@ -11,28 +100,12 @@ function callYoutubeSearchApi(artist, track) {
     var song = response.items[0];
     var id = song.id.videoId;
     console.log("Video ID: " + id);
-    var data = $("<td>")
-    var link = $("<a>");
-    link.attr("href", "https://www.youtube.com/watch?v=" + id);
-    link.attr("target", "_blank")
-    link.addClass("col-4");
-
-  //  var title = $("<p>");
-    //title.addClass("col-2")
-   // link.text(song.snippet.title);
-    var img = $("<img>");
-    img.addClass("img-responsive");
-    img.attr("src", song.snippet.thumbnails.default.url);
-    //link.append(title);
-    
-    link.append(img);
-    data.append(link);
-    $("#trackName").prepend(data);
+    links.push(id);
+    addStorage();
+    displayHistory();
     player.loadVideoById(id);
-    
-  
-  });
 
+  });
 
 }
 
@@ -77,13 +150,13 @@ $(document).on("click", "#submit", function (e) {
   console.log("press");
   var artist = $("#artist").val().trim();
   var track = $("#track").val().trim();
-  
+
   callYoutubeSearchApi(artist, track);
- 
+
   //getTrackId();
 
 });
 
-
-
-
+$(document).ready(function(){
+  displayHistory();
+})
